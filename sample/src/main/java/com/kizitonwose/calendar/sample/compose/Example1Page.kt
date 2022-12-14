9b8c9165ac14b6569229765294e46b7f4db93f8d
Kizito Nwose <kizitonwose@gmail.com>
2022-12-14 18:33:01 +0100

    Animate calendar height changes.

--- sample/src/main/java/com/kizitonwose/calendar/sample/compose/Example1Page.kt
+++ sample/src/main/java/com/kizitonwose/calendar/sample/compose/Example1Page.kt
@@ -1,8 +1,9 @@
 package com.kizitonwose.calendar.sample.compose
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
@@ -10,21 +11,28 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kizitonwose.calendar.compose.HorizontalCalendar
@@ -59,10 +67,10 @@ fun Example1Page(adjacentMonths: Long = 500) {
             firstDayOfWeek = daysOfWeek.first(),
         )
         val coroutineScope = rememberCoroutineScope()
-        val visibleMonth = rememberFirstMostVisibleMonth(state, viewportPercent = 90f)
+        val mostVisibleMonth = rememberFirstMostVisibleMonth(state)
         SimpleCalendarTitle(
             modifier = Modifier.padding(vertical = 10.dp, horizontal = 8.dp),
-            currentMonth = visibleMonth.yearMonth,
+            currentMonth = mostVisibleMonth.yearMonth,
             goToPrevious = {
                 coroutineScope.launch {
                     state.animateScrollToMonth(state.firstVisibleMonth.yearMonth.previousMonth)
@@ -74,30 +82,52 @@ fun Example1Page(adjacentMonths: Long = 500) {
                 }
             },
         )
-        MonthHeader(daysOfWeek = daysOfWeek)
-        BoxWithConstraints {
-            HorizontalCalendar(
-                modifier = Modifier
-                    .height((maxWidth / 7) * visibleMonth.weekDays.count()),
-                state = state,
-                dayContent = { day ->
-                    Day(day, isSelected = selections.contains(day)) { clicked ->
-                        if (selections.contains(clicked)) {
-                            selections.remove(clicked)
-                        } else {
-                            selections.add(clicked)
-                        }
+
+        val density = LocalDensity.current
+        var headerSize by remember { mutableStateOf(DpSize.Zero) }
+        val calendarHeight by animateDpAsState(
+            headerSize.height + (headerSize.width / 7) * mostVisibleMonth.weekDays.count(),
+            tween(durationMillis = 250),
+        )
+        HorizontalCalendar(
+            modifier = Modifier
+                .background(Color.Blue.copy(alpha = 0.1f)) // Only needed to visualize the content.
+                .height(calendarHeight),
+            state = state,
+            dayContent = { day ->
+                Day(day, isSelected = selections.contains(day)) { clicked ->
+                    if (selections.contains(clicked)) {
+                        selections.remove(clicked)
+                    } else {
+                        selections.add(clicked)
                     }
-                },
-            )
-        }
+                }
+            },
+            monthHeader = {
+                MonthHeader(
+                    modifier = Modifier
+                        .wrapContentHeight()
+                        .fillMaxWidth()
+                        .onSizeChanged {
+                            val size = density.run { DpSize(it.width.toDp(), it.height.toDp()) }
+                            if (headerSize != size) {
+                                headerSize = size
+                            }
+                        },
+                    daysOfWeek = daysOfWeek,
+                )
+            },
+        )
     }
 }
 
 @Composable
-private fun MonthHeader(daysOfWeek: List<DayOfWeek>) {
+private fun MonthHeader(
+    modifier: Modifier = Modifier,
+    daysOfWeek: List<DayOfWeek>,
+) {
     Row(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .testTag("MonthHeader"),
     ) {
